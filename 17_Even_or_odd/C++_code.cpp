b98b2cdcac68df1f431c885e4cf45f1065f242f7
Debosmita Pal <debosmitapal429@gmail.com>
2025-08-03 15:56:33 +0530

    Adding odd/even program

---
+++ 17_Even_or_odd/C++_code.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+bool isEven(int num) {
+    return (num & 1) == 0;
+}
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    if (isEven(num)) {
+        cout << num << " is even." << endl;
+    } else {
+        cout << num << " is odd." << endl;
+    }
+    return 0;
+} 
