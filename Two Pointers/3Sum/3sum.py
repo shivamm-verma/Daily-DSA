d8e374cf6fd5d829b5f0c3748302b437d214714f
nilesh325 <saraswatnilesh3@gmail.com>
2025-08-10 17:58:27 +0530

    Fixes #203

---
+++ Two Pointers/3Sum/3sum.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def threeSum(self, nums):
+        nums.sort()
+        lst = []
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            j = i + 1
+            k = len(nums) - 1
+            while j < k:
+                total = nums[i] + nums[j] + nums[k]
+                if total == 0:
+                    lst.append([nums[i], nums[j], nums[k]])
+                    j += 1
+                    k -= 1
+                    while j < k and nums[j] == nums[j - 1]:
+                        j += 1
+                    while j < k and nums[k] == nums[k + 1]:
+                        k -= 1
+                elif total > 0:
+                    k -= 1
+                else:
+                    j += 1
+        return lst
+
+
+if __name__ == "__main__":
+    try:
+        user_input = input("Enter a list of integers separated by spaces: ")
+        nums = list(map(int, user_input.strip().split()))
+        solution = Solution()
+        result = solution.threeSum(nums)
+        print("Triplets that sum to zero:", result)
+    except ValueError:
+        print("❌ Invalid input. Please enter integers only.")
---
+++ Two Pointers/3Sum/Main.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 🧮 Accept user input
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+        System.out.println("Enter " + n + " integers separated by space:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        // 🔍 Call threeSum
+        Solution sol = new Solution();
+        List<List<Integer>> result = sol.threeSum(nums);
+
+        // 📤 Print result
+        System.out.println("Triplets that sum to zero:");
+        for (List<Integer> triplet : result) {
+            System.out.println(triplet);
+        }
+
+        sc.close();
+    }
+}
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int j = i + 1;
+            int k = nums.length - 1;
+
+            while (j < k) {
+                int total = nums[i] + nums[j] + nums[k];
+                if (total == 0) {
+                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+                    while (j < k && nums[j] == nums[j - 1]) j++;
+                    while (j < k && nums[k] == nums[k + 1]) k--;
+                } else if (total < 0) {
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
+        return res;
+    }
+}
+
