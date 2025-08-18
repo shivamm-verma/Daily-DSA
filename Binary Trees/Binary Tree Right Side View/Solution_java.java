aea753e071257ea1082c7e1c5c3cbf6de2ede243
aditya singh rathore <142787780+Adez017@users.noreply.github.com>
2025-08-18 23:05:28 +0530

    Update Solution_java.java

--- Binary Trees/Binary Tree Right Side View/Solution_java.java
+++ Binary Trees/Binary Tree Right Side View/Solution_java.java
@@ -21,14 +21,6 @@ class Solution {
 
             for (int i = 0; i < size; i++) {
                 TreeNode node = q.poll();
-                if (node.right != null) q.offer(node.right);
-                if (node.left != null) q.offer(node.left);
-                level.add(node.val);
-            }
-
-
-            for (int i = 0; i < size; i++) {
-                TreeNode node = q.poll();
                 if (i == 0) res.add(node.val); // first node at this level is rightmost
                 if (node.right != null) q.offer(node.right);
                 if (node.left != null) q.offer(node.left);
