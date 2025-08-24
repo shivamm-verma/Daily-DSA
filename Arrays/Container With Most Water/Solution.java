26e844440e86194b33ed26621ff51011a3a7a409
Khushi-verma1504 <khushiverma1504@gmail.com>
2025-08-24 16:52:22 +0530

    Added new problem: Container with most water along with Markdown.md

---
+++ Arrays/Container With Most Water/Solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+  public int maxArea(int[] height) {
+        int left=0, right=height.length-1;
+        int area=0, max=0;
+        while(left<right)
+        {
+            area=(right-left)*Math.min(height[left],height[right]);
+            max=Math.max(max,area);
+            if(height[right]>height[left])
+            {
+                left++;
+            }else
+            {
+                right--;
+            }
+        }
+        return max;
+    }
+}
