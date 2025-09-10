bfb320318f37bb94d31e4b632b83fcd89ed08f3b
Debosmita Pal <debosmitapal429@gmail.com>
2025-09-10 10:06:37 +0530

    Adding in a folder

--- Dynamic Programming/2D DP/Wildcard Matching/Javacode.java
+++ Dynamic Programming/2D DP/Wildcard Matching/Javacode.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class WildcardMatching {
+public class Javacode {
     public static boolean isMatch(String s, String p) {
         int n = s.length(), m = p.length();
         boolean[][] dp = new boolean[n + 1][m + 1];
