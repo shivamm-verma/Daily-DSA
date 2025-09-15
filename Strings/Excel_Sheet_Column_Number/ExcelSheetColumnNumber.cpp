3d2fdd9bda70d3c94d750dd7f599ead71ffded24
DHARANEE DHARAN V <142580160+dharaneedharan-v@users.noreply.github.com>
2025-09-16 02:04:06 +0530

    Excel Sheet Column Number (#360)

---
+++ Strings/Excel_Sheet_Column_Number/ExcelSheetColumnNumber.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    int titleToNumber(string columnTitle) {
+        int result = 0;
+        for (char c : columnTitle) {
+            result = result * 26 + (c - 'A' + 1);
+        }
+        return result;
+    }
+};
+
+int main() {
+    string columnTitle;
+    cout << "Enter Excel column title (e.g., 'AB'): ";
+    cin >> columnTitle;
+    
+    Solution solution;
+    cout << "Column Number is: " << solution.titleToNumber(columnTitle) << endl;
+
+    return 0;
+}
